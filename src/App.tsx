5a21d9c01f9d2368ba6873a7bbe4b3654c63b53b
王唯佳 <wqywangweijia@gmail.com>
2020-02-01 10:21:18 +0800

    目录添加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Menu } from 'antd';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Menu
+          theme="dark"
+          style={{ width: 256 }}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
         >
-          Learn React
-        </a>
-      </header>
+          <Menu.Item key="1">Option 1</Menu.Item>
+        </Menu>
     </div>
   );
 }
